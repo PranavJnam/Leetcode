d55f6e4c38918d126af9dc9201bd84f4f0716bc1
PranavJ <pranavjnam@gmail.com>
2025-06-28 23:07:05 +0530

    Initial commit: merge alternately Java solution

---
+++ MergeStrings.java
@@ -0,0 +1,25 @@
+public class MergeStrings {
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+                merged.append(word1.charAt(i));
+                i++;
+            }
+            if (j < word2.length()) {
+                merged.append(word2.charAt(j));
+                j++;
+            }
+        }
+
+        return merged.toString();
+    }
+
+    public static void main(String[] args) {
+        String word1 = "abc";
+        String word2 = "pqr";
+        String result = mergeAlternately(word1, word2);
+        System.out.println(result);  // Output: "apbqcr"
+    }
+}
